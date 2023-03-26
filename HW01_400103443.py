3cc930af9a04ecbda248231408364176a81f3e1d
HosseinShadman <128729049+HosseinShadman@users.noreply.github.com>
2023-03-26 16:32:40 +0330

    defining parent class

--- HW01_400103443.py
+++ HW01_400103443.py
@@ -1,1 +1,25 @@
 # AP-HW01 Hossein Shadman 400103443
+
+#Class
+class Animal:
+    def __init__(self,name,age,gender,breed,color):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.breed = breed
+        self.color = color
+        
+    def namedescription(self):
+        return f"my name is {self.name}"
+    
+    def agedescription(self):
+        return f"I am {self.age}"
+    
+    def genderdescription(self):
+        return f"my gender is {self.gender}"
+    
+    def breeddescription(self):
+        return f"my breed is {self.breed}"
+    
+    def colordescription(self):
+        return f"my color is {self.color}"
